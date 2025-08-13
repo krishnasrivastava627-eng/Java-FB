bd7f6c50c6b5b131618d047b240af3283478fe97
Krishna Srivastava_0848 <krishnasrivastava627@gmail.com>
2025-08-13 14:22:38 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,10 @@
+public class arr {
+    public static void main(String[] args){
+        int arr[]= { 1,7,9,3,4,5};
+
+        int n = arr.length;
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i] + " ");
+
+    }
+}
