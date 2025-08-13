8dd1e28d45a6af5c31d0ab70b2b7a7c0b00d9f57
Krishna Srivastava_0848 <krishnasrivastava627@gmail.com>
2025-08-13 14:48:16 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,11 @@
+public class assignment {
+    public static void main(String[] args){
+        // Assignment operators
+        int f = 7;
+        System.out.println("f += 3: " + (f += 3));
+        System.out.println("f -= 2: " + (f -= 2));
+        System.out.println("f *= 4: " + (f *= 4));
+        System.out.println("f /= 3: " + (f /= 3));
+        System.out.println("f %= 2: " + (f %= 2));
+    }
+}
