714d74e077dc6d97600a31e8bf239bf8e497df65
Krishna Srivastava_0848 <krishnasrivastava627@gmail.com>
2025-08-13 13:22:16 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,21 @@
+public class data{
+    public static void main(String[] args){
+        
+        System.out.println("Range "+ Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+
+        System.out.println("Range "+ Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+
+        System.out.println("Range "+ Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+        System.out.println("Range "+ (int)Character.MIN_VALUE + " to " + (int)Character.MAX_VALUE); 
+
+        System.out.println("Boolean values: " + Boolean.FALSE + " to " + Boolean.TRUE);
+
+        System.out.println("Range "+ Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+
+        System.out.println("Range "+ Double.MIN_VALUE + " to " + Double.MAX_VALUE); 
+
+        System.out.println("Range "+ Short.MIN_VALUE + " to " + Short.MAX_VALUE);  
+
+    }
+}
